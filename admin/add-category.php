e13ca9b8e5e00e991602e74049c52d6d23c75127
Emily Ahmad <126031180+emilyahmad@users.noreply.github.com>
2026-02-17 08:55:29 -0500

    2/17

--- admin/add-category.php
+++ admin/add-category.php
@@ -60,3 +60,67 @@ if (isasset($_POST['active'])) {
 } else {
     $active = "no";
 }
+
+if ($image_name != " ") {
+    // upload image if selected
+
+    // auto rename image
+    // get extension
+    $ext = end(explode('.', $image_name));
+
+    // rename image
+    $image_name = $_FILES['image']['tmp_name'];
+
+    $destination_path = "../images/category/".$image_name;
+
+    // finally upload image
+    $upload = move_uploaded_file($source_path, $destination_path);
+
+    // check whether image is uploaded or not
+    // if image is not uploaded, stop process & redirect error message
+
+    if ($upload == false) {
+        // set message
+        $_SESSION['upload'] = "<div class='error'>Failed to upload image.</div>";
+
+        // redirect to add category page
+        header('locatoin: '.SITEURL.'admin/add-category.php');
+
+        // stop process
+        die();
+    }
+
+} // image is not blank
+
+} // additional end bracket for if file is not set
+else {
+    // dont upload image & set value blank
+    $image_name = "";
+}
+
+// create SQL query to insert category to database
+$sql = "INSERT INTO tbl_category SET title='$title', image_name='$image_name',
+featured='$featured',
+active='$active'";
+
+// execute query & save  in database
+$res = mysqli_query($conn, $sql);
+
+// check whether the query executed or not & data added or not
+if ($res == true) {
+    // query executed & category added
+    $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
+    // redirect to manage category page
+    header('location:' .SITEURL.'admin/add-category.php');
+} else {
+    // failed
+    $_SESSION['add'] = "<div class='error'>Failed to Add Category.</div>";
+
+    // redirect to manage category page
+    header('location:' .SITEURL.'admin/add-category.php');
+} // should have additional end bracket
+}
+?>
+</div>
+</div>
+<?php include('partials-front/footer.php');
